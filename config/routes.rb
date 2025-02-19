3f304f8fc11b3e17ce817bc57b8997c88994862d
michele di stadio <distadiomichele93@gmail.com>
2025-02-19 11:44:29 +0100

    orders

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   namespace :admin do
+    resources :orders
     resources :products do
       resources :stocks, only: [:index, :new, :show, :create, :edit, :update, :destroy]
     end
