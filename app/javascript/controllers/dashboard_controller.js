dc871f678d5623e421f8cbc662fce0889a0983b6
michele di stadio <distadiomichele93@gmail.com>
2025-02-19 15:42:02 +0100

    graphic dashboard

---
+++ app/javascript/controllers/dashboard_controller.js
@@ -0,0 +1,49 @@
+import { Controller } from "@hotwired/stimulus"
+import { Chart, registerables } from 'chart.js';
+
+Chart.register(...registerables);
+
+// Connects to data-controller="dashboard"
+export default class extends Controller {
+  static values = { revenue: Array }
+  initialize() {
+    const data = this.revenueValue.map((item) => item[1]/100.0)
+    const labels = this.revenueValue.map((item) => item[0])
+
+    const ctx = document.getElementById('revenueChart').getContext('2d');
+    new Chart(ctx, {
+      type: 'line',
+      data: {
+        labels: labels,
+        datasets: [{
+          label: 'Revenue $',
+          data: data,
+          borderColor: 'rgba(75, 192, 192, 1)',
+          borderWidth: 3,
+          fill: true,
+        }]
+      },
+      options: {
+        plugins: {
+          legend: {
+            display: false
+          }
+        },
+        scales: {
+          x: {
+            grid: {
+              display: false
+            }
+          },
+          y: {
+            beginAtZero: true,
+            grid: {
+              color: '#d4f3ef',
+              borderDash: [5, 5]
+            }
+          }
+        }
+      }
+    });
+  }
+}
