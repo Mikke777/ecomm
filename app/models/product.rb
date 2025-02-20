e4a339279ac7caf02ab459ff24d92c006c213be1
michele di stadio <distadiomichele93@gmail.com>
2025-02-20 12:13:45 +0100

    show page for the category

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,9 @@
 class Product < ApplicationRecord
   belongs_to :category
-  has_many_attached :images
+  has_many_attached :images do |attachable|
+    attachable.variant :medium, resize: "250x250"
+  end
+
   has_many :stocks, dependent: :destroy
   has_many :order_products
 end
