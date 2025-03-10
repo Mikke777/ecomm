fff7abd3f27444aca3436e9e78a55e13acc57cc6
michele di stadio <distadiomichele93@gmail.com>
2025-03-10 16:50:33 +0100

    added validations

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,16 @@
 class Category < ApplicationRecord
   has_one_attached :image
   has_many :products
+
+  validates :name, presence: true
+  validates :description, presence: true
+  validates :image, presence: true
+  validate :image_size
+
+
+  def image_size
+    if image.attached? && image.blob.byte_size > 200.kilobytes
+      errors.add(:photo, "size should be less than 200KB")
+    end
+  end
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -6,4 +6,25 @@ class Product < ApplicationRecord
 
   has_many :stocks, dependent: :destroy
   has_many :order_products
+
+  validates :name, presence: true
+  validates :description, presence: true
+  validates :price, presence: true
+  validates :category_id, presence: true
+  validates :images, presence: true
+  validate :images_size
+  validates :price, numericality: { greater_than: 0 }
+  validates :active, inclusion: { in: [true, false] }
+
+
+
+  def images_size
+    if images.attached?
+      images.each do |image|
+        if image.blob.byte_size > 200.kilobytes
+          errors.add(:images, "size should be less than 200KB")
+        end
+      end
+    end
+  end
 end
